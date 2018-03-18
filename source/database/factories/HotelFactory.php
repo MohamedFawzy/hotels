48ccc1c99b543bee88962dc660d28389dbe248cc
Mohamed Fawzy <mfawzy22@gmail.com>
2018-03-19 00:56:26 +0200

    update factory hotel

--- source/database/factories/HotelFactory.php
+++ source/database/factories/HotelFactory.php
@@ -7,5 +7,16 @@ $factory->define(App\Hotel::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'price' => $faker->randomFloat(2,50,200),
+        'city'  => $faker->city,
+        'availability' => [
+            [
+                'from' => $faker->date('d-m-Y'),
+                'to'   => $faker->date('d-m-Y')
+            ],
+            [
+                'from' => $faker->date('d-m-Y'),
+                'to'   => $faker->date('d-m-Y')
+            ]
+        ]
     ];
 });
