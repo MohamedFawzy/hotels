5062a5bcd50b5b52bfa458ddfbdbb4a8566f7d9e
Mohamed Fawzy <mfawzy22@gmail.com>
2018-03-20 02:13:44 +0200

    update test case for index

--- source/tests/Unit/app/Http/Controllers/Api/V1/HotelsControllerTest.php
+++ source/tests/Unit/app/Http/Controllers/Api/V1/HotelsControllerTest.php
@@ -4,7 +4,7 @@ use Tests\TestCase;
 class HotelsControllerTest extends TestCase
 {
 
-    const ROUTE="api/v1/hotels?column=name&direction=asc&page=1&per_page=15&search_column=id&search_operator=equal&search_input=";
+    const ROUTE="/api/v1/hotels?column=name&direction=asc&page=1&per_page=15&search_column=id&search_operator=equal&search_input=";
 
     public function testIndexHttpStatusCode()
     {
