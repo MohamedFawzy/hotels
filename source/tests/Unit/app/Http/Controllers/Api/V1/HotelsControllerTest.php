39f24fbc8358621828951013938303c242477305
Mohamed Fawzy <mfawzy22@gmail.com>
2018-03-20 02:30:57 +0200

    revamp controller test

--- source/tests/Unit/app/Http/Controllers/Api/V1/HotelsControllerTest.php
+++ source/tests/Unit/app/Http/Controllers/Api/V1/HotelsControllerTest.php
@@ -4,4 +4,10 @@ use Tests\TestCase;
 class HotelsControllerTest extends TestCase
 {
 
+    //const ROUTE="/api/v1/hotels?column=name&direction=asc&page=1&per_page=15&search_column=id&search_operator=equal&search_input=";
+
+    public function testIndexHttpStatusCode()
+    {
+        $this->assertEquals(5,5);
+    }
 }
