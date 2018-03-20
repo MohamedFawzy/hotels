a26a07921ca86ad61a5d562b6adbbe310c4a13bb
Mohamed Fawzy <mfawzy22@gmail.com>
2018-03-20 13:55:47 +0200

    update status code

--- source/tests/Unit/app/Http/Controllers/Api/V1/HotelsControllerTest.php
+++ source/tests/Unit/app/Http/Controllers/Api/V1/HotelsControllerTest.php
@@ -5,12 +5,14 @@ use Faker\Generator as Faker;
 class HotelsControllerTest extends TestCase
 {
 
-    //const ROUTE="/api/v1/hotels?column=name&direction=asc&page=1&per_page=15&search_column=id&search_operator=equal&search_input=";
+    const ROUTE="/api/v1/hotels?column=name&direction=asc&page=1&per_page=15&search_column=id&search_operator=equal&search_input=";
 
     public function testIndexHttpStatusCode()
     {
         factory(App\Hotel::class, 100)->create();
 
-        $this->assertEquals(5,5);
+        $request = $this->call('GET', self::ROUTE);
+        $statusCode = $request->getStatusCode();
+        $this->assertEquals(200, $statusCode);
     }
 }
