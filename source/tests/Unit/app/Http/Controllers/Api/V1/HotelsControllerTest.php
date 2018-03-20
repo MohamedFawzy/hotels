e66847883a953d10907a57e01ece9d1802b1ea21
Mohamed Fawzy <mfawzy22@gmail.com>
2018-03-20 02:23:11 +0200

    update test controller

--- source/tests/Unit/app/Http/Controllers/Api/V1/HotelsControllerTest.php
+++ source/tests/Unit/app/Http/Controllers/Api/V1/HotelsControllerTest.php
@@ -4,12 +4,4 @@ use Tests\TestCase;
 class HotelsControllerTest extends TestCase
 {
 
-    const ROUTE="/api/v1/hotels?column=name&direction=asc&page=1&per_page=15&search_column=id&search_operator=equal&search_input=";
-
-    public function testIndexHttpStatusCode()
-    {
-        $result     = $this->call('GET', self::ROUTE);
-        $statusCode = $result->getStatusCode();
-        $this->assertEquals(200, $statusCode);
-    }
 }
