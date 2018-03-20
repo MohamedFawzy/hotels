1ab06d6143ec5e1a766972c2e46f35f706ba9888
Mohamed Fawzy <mfawzy22@gmail.com>
2018-03-20 11:59:39 +0200

    update gitignore

--- source/tests/Unit/app/Http/Controllers/Api/V1/HotelsControllerTest.php
+++ source/tests/Unit/app/Http/Controllers/Api/V1/HotelsControllerTest.php
@@ -7,27 +7,6 @@ class HotelsControllerTest extends TestCase
 
     //const ROUTE="/api/v1/hotels?column=name&direction=asc&page=1&per_page=15&search_column=id&search_operator=equal&search_input=";
 
-
-    public function __construct()
-    {
-        for($i=0; $i<100; $i++){
-            $faker = new Faker();
-            $hotel = new \App\Hotel();
-
-            $hotel->name = $faker->name;
-            $hotel->price = $faker->price;
-            $hotel->city = $faker->city;
-            $hotel->availability = [
-              [
-                  'from' => $faker->date('d-m-Y'),
-                  'to'   => $faker->date('d-m-Y')
-              ]
-            ];
-
-            $hotel->save();
-        }
-    }
-
     public function testIndexHttpStatusCode()
     {
         $this->assertEquals(5,5);
