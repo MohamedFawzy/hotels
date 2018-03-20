74737c60bf2b85d104255520a406115b4cffe1fd
Mohamed Fawzy <mfawzy22@gmail.com>
2018-03-20 11:42:49 +0200

    add factory for test cases

--- source/tests/Unit/app/Http/Controllers/Api/V1/HotelsControllerTest.php
+++ source/tests/Unit/app/Http/Controllers/Api/V1/HotelsControllerTest.php
@@ -6,6 +6,12 @@ class HotelsControllerTest extends TestCase
 
     //const ROUTE="/api/v1/hotels?column=name&direction=asc&page=1&per_page=15&search_column=id&search_operator=equal&search_input=";
 
+
+    public function __construct()
+    {
+        factory(App\Hotel::class, 100)->create();
+    }
+
     public function testIndexHttpStatusCode()
     {
         $this->assertEquals(5,5);
